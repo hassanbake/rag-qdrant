c409ba00a4d7f991dfaf6cee575328491bffdf5e
Maherolnaghsh Rouzbeh <R.Maherolnaghsh@etadbir.com>
2024-01-15 15:43:21 +0330

    loading pdf file

--- ingest.py
+++ ingest.py
@@ -1,1 +1,7 @@
+from langchain_community.vectorstores import Qdrant
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+loader = PyPDFLoader("data.pdf")
+document = loader.load()
